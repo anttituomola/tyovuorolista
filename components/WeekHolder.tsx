8f6ac73144cc8722cf29058f8d7b1242d5fd65d5
Antti <anttituomola8@gmail.com>
2022-07-27 13:57:17 +0300

    Start from scratch

--- components/WeekHolder.tsx
+++
@@ -1,9 +0,0 @@
-import Day from './Day'
-
-type Props = {}
-const WeekHolder = (props: Props) => {
-  return (
-    <Day />
-  )
-}
-export default WeekHolder
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import WeekHolder from '../components/WeekHolder'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <WeekHolder />
+      Kalenteri
     </div>
   )
 }
